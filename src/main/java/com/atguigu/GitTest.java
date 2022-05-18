5f5886af072b0a6071484dd720c5a602c8636a6b
git <git@qq.com>
2022-05-18 15:25:19 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,6 +7,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
